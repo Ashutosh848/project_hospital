ab4f3aa0845ed7057ee9866dae57fb80b5ca2829
Ashutosh848 <ashu.tiwari.9741@gmail.com>
2025-08-30 11:39:10 +0530

    changes

--- hospital_claims_backend/start.sh
+++ hospital_claims_backend/start.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 cd hospital_claims
+
+# Run migrations
+echo "Running migrations..."
+python manage.py migrate
+
+# Collect static files
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
+# Start the server
+echo "Starting server..."
 gunicorn hospital_claims.wsgi:application --bind 0.0.0.0:$PORT --workers 2 --timeout 120
   
